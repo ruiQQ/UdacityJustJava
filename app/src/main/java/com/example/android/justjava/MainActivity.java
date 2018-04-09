4a06433af6440fbfface6a1c372a90133d770c87
Rui <ruiqiu15@gmail.com>
2018-04-09 00:34:54 -0700

    added feature to open email app

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.justjava;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -45,19 +47,27 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        EditText nameField = (EditText)findViewById(R.id.name_field);
+        EditText nameField = (EditText) findViewById(R.id.name_field);
         String name = nameField.getText().toString();
 
-        CheckBox creamBox = (CheckBox)findViewById(R.id.whipped_cream_checkbox);
+        CheckBox creamBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = creamBox.isChecked();
 
-        CheckBox chocoBox = (CheckBox)findViewById(R.id.chocolate_checkbox);
+        CheckBox chocoBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate = chocoBox.isChecked();
 
         int price = calculatePrice(hasWhippedCream, hasChocolate);
-        String priceMessage = createOrderSummary(name, price , hasWhippedCream, hasChocolate);//"Total: $" + quantity*5 + "\nThank you!";
+        String priceMessage = createOrderSummary(name, price, hasWhippedCream, hasChocolate);//"Total: $" + quantity*5 + "\nThank you!";
 
-        displayMessage(priceMessage);
+        // open up email app
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java Order for" + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        
     }
 
     private int calculatePrice(boolean addCream, boolean addChoco) {
@@ -70,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         if (addChoco) {
             basePrice += 2;
         }
-        return quantity*basePrice;
+        return quantity * basePrice;
     }
 
     /**
@@ -101,11 +111,4 @@ public class MainActivity extends AppCompatActivity {
         return priceMessage;
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
 }
